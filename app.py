23e6f5dc323ad77db5d7e698aca5d030bfc475ed
Simone Brown <smb866@nyu.edu>
2019-12-08 23:53:49 -0500

    added additional extra feature and pw hashing

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
 import os
 import time
+import hashlib
 
+SALT = 'cs3083'
 
 #Initialize the app from Flask
 app = Flask(__name__)
@@ -39,13 +41,14 @@ def register():
 def loginAuth():
     #grabs information from the forms
     username = request.form['username']
-    password = request.form['password']
+    password = request.form['password'] + SALT
+    hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
     #cursor used to send queries
     cursor = conn.cursor()
     #executes query
     query = 'SELECT * FROM Person WHERE username = %s and password = %s'
-    cursor.execute(query, (username, password))
+    cursor.execute(query, (username, hashed_password))
     #stores the results in a variable
     data = cursor.fetchone()
     #use fetchall() if you are expecting more than 1 data row
@@ -66,7 +69,8 @@ def loginAuth():
 def registerAuth():
     #grabs information from the forms
     username = request.form['username']
-    password = request.form['password']
+    password = request.form['password'] + SALT
+    hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
     firstname = request.form['firstname']
     lastname = request.form['lastname']
     bio = request.form['bio']
@@ -86,7 +90,7 @@ def registerAuth():
         return render_template('register.html', error = error)
     else:
         ins = 'INSERT INTO Person VALUES(%s, %s, %s, %s, %s)'
-        cursor.execute(ins, (username, password, firstname, lastname, bio))
+        cursor.execute(ins, (username, hashed_password, firstname, lastname, bio))
         conn.commit()
         cursor.close()
         return render_template('index.html')
@@ -138,13 +142,14 @@ def upload_image():
 @app.route("/tag/<photoID>", methods=["GET", "POST"])
 def tag(photoID):
     cursor = conn.cursor() 
-    query = "INSERT INTO Tagged(username, photoID) VALUES(%s, %s)"
+    query = "INSERT INTO Tagged(username, photoID, tagstatus) VALUES(%s, %s, %s)"
     #add a check to see if username is valid and if the tag has been accepted
-    username = request.form['username']
-    cursor.execute(query,(username, photoID))
+    username_str = request.form['username']
+    username_lst = username_str.split(",")
+    for username in username_lst:
+        cursor.execute(query,(username, photoID, 1))
     conn.commit()
     cursor.close()
-    
     return home()
     
 # **FEATURE 1**
@@ -175,9 +180,9 @@ def image(photoID):
     cursor.execute(query, photoID)
     like = cursor.fetchall()
     
-    #retrieves all the usernames tagged in the photo
+    #retrieves all the usernames, first names, and last names of people tagged in the photo
     cursor = conn.cursor()
-    query = "SELECT * FROM Tagged WHERE photoID = %s"
+    query = "SELECT Tagged.username, Person.firstName, Person.lastName FROM Tagged NATURAL JOIN Person WHERE photoID = %s  AND Tagged.tagstatus = 1"
     cursor.execute(query, photoID)
     tag = cursor.fetchall()
     
@@ -185,7 +190,7 @@ def image(photoID):
 
 
 
-
+#extra feature 4: like and rate (Simone Brown smb866)
 @app.route("/likeandrate/<photoID>", methods=["GET", "POST"])
 def likeandrate(photoID):
     
@@ -196,24 +201,9 @@ def likeandrate(photoID):
     cursor.execute(query, (username, photoID, rating))
     conn.commit()
     cursor.close()
+    return redirect(url_for("images"))
 
-    return
-    #return render_template("home.html")
-    #return redirect(url_for('/images'))
-
-#extra feature 1: comment on the photo
-@app.route("/comment/<photoID>", methods=["GET", "POST"])
-def comment(photoID):
-    
-    username = session['username']
-    cursor = conn.cursor()
-    comment = request.form['comment']
-    query ='INSERT INTO Comments (username, photoID, comment) VALUES(%s, %s, %s)'
-    cursor.execute(query ,(username, photoID, comment))
-    conn.commit()
-    cursor.close()
-    return
-#extra feature 2: search by username
+#extra feature 2: search by username (Divya Madala dm3980)
 @app.route("/username", methods=["GET", "POST"])
 def username():
     return render_template("findUsername.html")
@@ -232,7 +222,90 @@ def findUsername():
     else:
         return render_template("findUsername.html", message = "couldn't find username")
     
+
+@app.route("/f", methods=["GET", "POST"])
+def follow():
+    return render_template("follow.html")
+
+#to follow a user
+@app.route("/follow", methods=["GET", "POST"])
+def updateFollowTable():
+    username_followed = request.form["username"]
+    username_follower = session["username"]
+    cursor = conn.cursor()
     
+    query1 = "SELECT username_followed FROM Follow WHERE username_follower = %s"
+    cursor.execute(query1, username_follower)
+    data = cursor.fetchall()
+    
+    #this if statement checks if the user is following anyone
+    if(data):
+        #this for loop goes over the rows returned by query
+        for dict in data:
+            username = dict["username_followed"]
+            # this checks if the user is already following the user
+            if(username == username_followed):
+                return render_template("follow.html", message="You are already following this user")
+
+    # if the user hasn't followed then this code will execute
+    query2 = "INSERT INTO Follow (username_followed, username_follower, followstatus) VALUES(%s, %s, %s)"
+    cursor.execute(query2, (username_followed, username_follower, 0))
+    conn.commit()
+    cursor.close()
+    return render_template("follow.html", message = "You have requested to follow " + username_followed + "\n")
+
+#view all of the people you are following
+@app.route("/following", methods=["GET", "POST"])
+def following():
+    my_username = session["username"]
+    cursor = conn.cursor()
+    query = "SELECT username_followed FROM Follow WHERE username_follower = %s AND followstatus = 1"
+    cursor.execute(query, my_username)
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template("following.html", follow=data)
+
+#Extra feature 3: unfollow (Simone Brown smb866)
+# When you unfollow a user, the row in the Follow table that represented you following that user will be deleted. I can't just get set to 0 because that is how we track unaccepted follow requests. We also decided that when you unfollow a user, you don't want them tagged in any of your posts anymore. To do this, we update the Tagged table to change the tagstatus from 1 to 0 for any of your posts that the user you're unfollowing is tagged in. To know which tagged photo you posted, we have to join Tagged with Photo on the photoID. Then we know which photos you posted that the user is tagged in.
+@app.route("/unfollow/<username>", methods=["GET", "POST"])
+def unfollow(username):
+    my_username = session["username"]
+    cursor = conn.cursor()
+    query = "DELETE FROM Follow WHERE username_followed = %s AND username_follower = %s"
+    cursor.execute(query, (username, my_username))
+    query2 = "UPDATE Tagged JOIN Photo ON Tagged.photoID = Photo.photoID SET tagstatus = 0 WHERE photoPoster = %s AND username = %s"
+    cursor.execute(query2, (my_username, username))
+    data = cursor.fetchall()
+    print(data)
+    conn.commit()
+    cursor.close()
+    return redirect("/following")
+
+#manage follows
+@app.route("/follow_requests", methods=["GET", "POST"])
+def followRequests():
+    my_username = session["username"]
+    cursor = conn.cursor()
+    query = "SELECT username_follower FROM Follow WHERE username_followed = %s AND followstatus = 0"
+    cursor.execute(query, my_username)
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template("followRequests.html", follow = data)
+
+@app.route("/follow_request_action/<username>/<status>", methods=["GET", "POST"])
+def followRequestAction(username, status):
+    my_username = session["username"]
+    cursor = conn.cursor()
+    if(status == "accept"):
+        query = "UPDATE Follow SET followstatus = 1 WHERE username_followed = %s AND username_follower = %s"
+    else:
+        query = "DELETE FROM Follow WHERE username_followed = %s AND username_follower = %s"
+    cursor.execute(query, (my_username, username))
+    conn.commit()
+    cursor.close()
+    return redirect("/home")
+
+#extra feature 1: search by tag (Divya Madala dm3980)
 @app.route("/tagged", methods=["GET", "POST"])
 def tagged():
     return render_template("findTag.html")
@@ -242,7 +315,7 @@ def tagged():
 def findTag():
     tagged = request.form['username']
     cursor = conn.cursor()
-    query = "SELECT photoID FROM Tagged WHERE username = %s"
+    query = "SELECT photoID FROM Tagged WHERE username = %s  AND tagstatus = 1"
     cursor.execute(query, tagged)
     data = cursor.fetchall()
     cursor.close()
